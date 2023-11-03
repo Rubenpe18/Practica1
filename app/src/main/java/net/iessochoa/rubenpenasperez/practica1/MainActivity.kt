8753395ba36340e193ee7ccf93a5a269b29c76b4
https://github.com/Rubenpe18/Practica1.git <ruben122aaa@gmail.com>
2023-11-03 19:08:52 +0100

    punto 46

--- app/src/main/java/net/iessochoa/rubenpenasperez/practica1/MainActivity.kt
+++ app/src/main/java/net/iessochoa/rubenpenasperez/practica1/MainActivity.kt
@@ -1,12 +1,41 @@
 package net.iessochoa.rubenpenasperez.practica1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etNombre:EditText
+    private lateinit var btAceptar:Button
+
+    fun enviarSaludo(){
+        //comprobamos si el campo está vacío
+        if (!etNombre.text.isEmpty()){
+            //Creamos el Intent hacia SaludoActivity
+            val intent = Intent(this,SaludoActivity::class.java)
+            //le pasamos el contenido etNombre como clave::valor(parecido a un map)
+            intent.putExtra(SaludoActivity.EXTRA_NOMBRE, etNombre.text.toString())
+            //iniciamos la activity SaludoActivity
+            startActivity(intent)
+        }else
+            //enviamos un mensaje por pantalla
+            Toast.makeText(this, "Es necesario introducir el nombre",Toast.LENGTH_LONG).show()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //buscamos los views
+        etNombre=findViewById(R.id.etNombre)
+        btAceptar=findViewById(R.id.btAceptar)
+
+        //asignamos acción como lambda
+        btAceptar.setOnClickListener{enviarSaludo()}
+
+
 
     }
 }
---
+++ app/src/main/java/net/iessochoa/rubenpenasperez/practica1/SaludoActivity.kt
@@ -0,0 +1,23 @@
+package net.iessochoa.rubenpenasperez.practica1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class SaludoActivity : AppCompatActivity() {
+    private lateinit var tvSaludo: TextView
+    companion object{
+        //Clave que permite recibir un texto que mostrará en pantalla
+        val EXTRA_NOMBRE="pactica1.SaludoActivity.nombre"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saludo)
+        //asignamos la instancia de tvSaludo en onCreate
+        tvSaludo= this.findViewById(R.id.tvSaludo)
+        //recuperamos el valor enviado por MainActivity
+        val nombre=intent.getStringExtra(EXTRA_NOMBRE)
+        //mostramos el valor
+        tvSaludo.text="Hola "+nombre+"!!"
+    }
+}
